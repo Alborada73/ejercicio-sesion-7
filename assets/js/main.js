f28f394c17c6949ec18424055042e0632168fc1b
yasna angulo <yasna.angulo>
2023-05-02 22:12:52 -0400

    subiendo assets e index

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+console.log("Funciones Cadenas de Caracteres")
+var nombreApellido = prompt("Ingrese su nombre y apellidos")
+
+console.log(nombreApellido.replaceAll("",""))
+console.log(nombreApellido.replaceAll("0","1"))
+console.log(nombreApellido.replaceAll("carlos","luis"))
+console.log(nombreApellido.replaceAll("0","1"))
+console.log(nombreApellido.replaceAll("é","e").replaceAll("i","i"))
+
+
+//Trim
+console.log(nombreApellido)
